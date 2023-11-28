530e619a53d93931d33eabdc52084c5f5ff953f1
Doğanay Balaban <doganaybalaban@192.168.1.42>
2023-11-28 23:44:35 +0300

    28.11.23

---
+++ ders07/doubleDataType/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$x = 13.5;
+$y = 13.5e3; #exponential form
+$z = ceil((0.1 + 0.2) * 10);
+# 4 döndürmesinin sebebi 0.1 + 0.2 = 0.3000000004
+
+
+echo "</br> ".$x ."</br> ". $y . "</br> " . $z."</br> ";
+
+$a = 0.23;
+$b = 1 - 0.77;
+
+if ($x==$y){
+    echo "yes";
+}else{
+    echo "no";
+}
+
+echo "</br> ";
+$f = 5;
+var_dump((float)$f);
